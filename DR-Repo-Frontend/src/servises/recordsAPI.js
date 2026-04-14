44d3d4d12b8defde1cab5797c78fe297787ddc3c
Emil S <41843174+OneHandedWonder@users.noreply.github.com>
2026-04-14 10:16:21 +0200

    Removed old folder.

--- DR-Repo-Frontend/src/servises/recordsAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5249/api/v2/records";
-
-export const getRecords = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
